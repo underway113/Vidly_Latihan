cda8a90763d39b6a435fc1a0d4a84a5fcd9b057d
BN000173816 <BN000173816@A455LN-LAPTOP>
2017-10-26 18:26:28 +0700

    Add project files.

---
+++ Vidly_Latihan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly_Latihan.MvcApplication" Language="C#" %>
---
+++ Vidly_Latihan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Latihan.Startup))]
+namespace Vidly_Latihan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
