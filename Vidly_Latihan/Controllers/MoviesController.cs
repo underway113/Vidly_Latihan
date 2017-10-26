f7557e3bb9ff8ecc91c547bb6057a431356c68af
Jeremy Adam <jeremyadam446@gmail.com>
2017-10-26 19:00:40 +0700

    add bootstrap-lumen

---
+++ Vidly_Latihan/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_Latihan.Models;
+
+namespace Vidly_Latihan.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            { 
+                name = "Frozne"
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly_Latihan/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly_Latihan.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+
+    // /movies/random
+
+
+}
